8c5716f58f3c9552eaf38e4078953693fdd21f67
Jose Marin <josermarinr18@gmail.com>
2020-11-29 21:05:52 +0100

    casi terminado, missing error in desktop and tablet with select the first colums and move tile

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,21 @@ import './App.css';
 import {Home} from './pages/Home'
 
 function App() {
+  let isDesktop = false;
+  if(window.innerWidth > 700){
+    return (
+      <div>
+        <h1 className={"titleWarning"}>Use Mobile for more performace, we work in desktop & Tablet version, 😊</h1>
+        <Home/>
+        <p className={"titleWarning"}>* problems with mouse and select object</p>
+      </div>
+     )
+   
+  }
+
   return (
     <div className="App">
-       <Home/>
+        <Home/>
     </div>
   );
 }
--- src/phaser/scene.ts
+++ src/phaser/scene.ts
@@ -16,36 +16,18 @@ export class MainScene extends Phaser.Scene {
   ];
 
   tileTypes = [
-    'bear',
-    'buffalo',
-    'chick',
-    'chicken',
-    'cow',
-    'crocodile',
-    'dog',
-    'duck',
-    'elephant',
-    'frog',
-    'giraffe',
-    'goat',
-    'gorilla',
-    'hippo',
-    'horse',
-    'monkey',
-    'moose',
-    'narwhal',
-    'owl',
-    'panda',
-    'parrot',
-    'penguin',
-    'pig',
-    'rabbit',
-    'rhino',
-    'sloth',
-    'snake',
-    'walrus',
-    'whale',
-    'zebra'
+    'dinoegg',
+    'diplodocusgirl',
+    'diplodocusmale',
+    'egg',
+    'pterodactilo',
+    'pterodactiloyellow',
+    'rexado',
+    'rexbaby',
+    'stegosaurus',
+    'stegosaurusdad',
+    'triceratopsgirl',
+    'triceratopsmale',
   ];
 
   activeTile1: Phaser.GameObjects.Sprite | null = null;
@@ -68,7 +50,7 @@ export class MainScene extends Phaser.Scene {
   random: Phaser.Math.RandomDataGenerator | null = null;
 
   matchParticles: Phaser.GameObjects.Particles.ParticleEmitterManager | null = null;
-  feedbox: Phaser.GameObjects.Image | null = null;
+  layer: Phaser.GameObjects.Image | null = null;
   scoreText: Phaser.GameObjects.Text | null = null;
   levelText: Phaser.GameObjects.Text | null = null;
 
@@ -77,18 +59,19 @@ export class MainScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.atlas('animals', 'assets/animals.png', 'assets/animals_atlas.json');
-    this.load.svg('back-fence', 'assets/back_fence.svg', { scale: 1 });
-    this.load.svg('feed', 'assets/feed.svg', { scale: 1 });
-    this.load.svg('feedbox', 'assets/feedbox.svg', { scale: 1 });
-    this.load.svg('front-fence', 'assets/front_fence.svg', { scale: 1 });
-    this.load.svg('refresh-sign', 'assets/refresh_sign.svg', { scale: 1 });
-    this.load.svg('title', 'assets/title_level_score.svg', { scale: 1 });
+    this.load.atlas('dinos', 'assets/dinos.png', 'assets/dinos_atlas.json');
+    this.load.svg('back-fence', 'assets/backFence.svg', { scale: 3 });
+    this.load.svg('meteor', 'assets/meteor.svg', { scale: 0.6 });
+    this.load.svg('layer', 'assets/Stonelayer.svg', { scale: 2 });
+    this.load.svg('front-fence', 'assets/frontFence.svg', { scale: 3 });
+    this.load.svg('bg', 'assets/bg.svg', { scale: 2 });
+    this.load.svg('reload', 'assets/refresh.svg', { scale: 2});
+    this.load.svg('title', 'assets/title.svg', { scale: 2.5 });
     this.load.image('match-particle', 'assets/white_particle.png');
   }
 
   create() {
-    this.cameras.main.setBackgroundColor('#49a139');
+    this.cameras.main.setBackgroundColor('#772A1C')
 
     this.tileWidth = this.game.scale.gameSize.width / 6;
     this.tileHeight = this.game.scale.gameSize.width / 6;
@@ -107,54 +90,67 @@ export class MainScene extends Phaser.Scene {
       on: false
     });
 
-    this.add.rectangle(0, 0, this.game.scale.gameSize.width, this.yOffset - this.tileHeight, 0x9ef1ff).setOrigin(0);
-    const title = this.add.text(120, 16* (this.game.scale.gameSize.width / 515), 'fuking test',
-      {
-        align: 'left',
-        color: '#000000',
-        fontSize: '40px',
-        stroke: '#000000',
-        strokeThickness: 1
-      })
-    .setOrigin(0.5, 0)
-    .setScale(this.game.scale.gameSize.width / 515)
-    .setDepth(1);
-    // const title = this.add.image(this.game.scale.gameSize.width / 2, 0, 'title').setOrigin(0.5, 0)
+    const sky = this.add.rectangle(0, 0, this.game.scale.gameSize.width, this.yOffset - this.tileHeight /2 +30, 0x9ef1ff).setOrigin(0); 
+    // const title = this.add.text(120, 16* (this.game.scale.gameSize.width / 515), 'fuking test',
+    //   {
+    //     align: 'left',
+    //     color: '#000000',
+    //     fontSize: '40px',
+    //     stroke: '#000000',
+    //     strokeThickness: 1
+    //   })
+    // .setOrigin(0.5, 0)
     // .setScale(this.game.scale.gameSize.width / 515)
     // .setDepth(1);
+    const title = this.add.image(this.game.scale.gameSize.width / 2, 0, 'title').setOrigin(0.5, 0)
+    .setScale(this.game.scale.gameSize.width / 515)
+    .setDepth(1);
+    
+    const scoreLayer = this.add.image(111 * (this.game.scale.gameSize.width / 515), 168 * (this.game.scale.gameSize.width / 515), 'layer')
+    .setOrigin(0.5)
+    .setScale(this.game.scale.gameSize.width / 1021)
+    .setDepth(2);
 
     this.scoreText = this.add
-    .text(111 * (this.game.scale.gameSize.width / 515), 168 * (this.game.scale.gameSize.width / 515), 'Score: 0',
+    .text(111 * (this.game.scale.gameSize.width / 515), 168 * (this.game.scale.gameSize.width / 515), 'Puntos: 0',
       {
         align: 'center',
         fontSize: '26px',
+        color: '#000000',
         stroke: '#000000',
         strokeThickness: 1
       })
     .setOrigin(0.5)
     .setScale(this.game.scale.gameSize.width / 515)
+    .setDepth(2);
+
+    const levelLayer = this.add.image(407 * (this.game.scale.gameSize.width / 515), 168 * (this.game.scale.gameSize.width / 515), 'layer')
+    .setOrigin(0.5)
+    .setScale(this.game.scale.gameSize.width / 1021)
     .setDepth(2);
 
     this.levelText = this.add
-    .text(407 * (this.game.scale.gameSize.width / 515), 168 * (this.game.scale.gameSize.width / 515), 'Level: 1',
+    .text(407 * (this.game.scale.gameSize.width / 515), 168 * (this.game.scale.gameSize.width / 515), 'Nivel: 1',
       {
         align: 'center',
         fontSize: '26px',
+        color: '#000000',
         stroke: '#000000',
         strokeThickness: 1
       })
     .setOrigin(0.5)
     .setScale(this.game.scale.gameSize.width / 515)
     .setDepth(2);
-
-    this.add.image(0, this.yOffset - this.tileHeight / 2, 'back-fence').setOrigin(0).setScale(this.game.scale.gameSize.width / 1021);
-    this.add.image(0, this.yOffset + this.tileHeight * 6, 'front-fence')
+      //todo change the alfa of bg and put more shadow in grid of tile
+    const bg = this.add.image(0,this.yOffset - this.tileHeight / 1.6, 'bg').setOrigin(0).setScale(this.game.scale.gameSize.width/750).setAlpha(0.5)
+    this.add.image(-20, this.yOffset - this.tileHeight / 2, 'back-fence').setOrigin(0).setScale(this.game.scale.gameSize.width / 1021);
+    this.add.image(-10, this.yOffset + this.tileHeight * 6, 'front-fence')
     .setOrigin(0).setScale(this.game.scale.gameSize.width / 1021);
     
-    this.feedbox = this.add.image(0, this.game.scale.gameSize.height - 5, 'feedbox')
+    this.layer = this.add.image(0, this.game.scale.gameSize.height - 5, 'layer')
     .setOrigin(0, 1).setScale((this.game.scale.gameSize.width / 6) / 199 * 1.7);
 
-    const restartSign = this.add.image(this.game.scale.gameSize.width - 5, this.game.scale.gameSize.height, 'refresh-sign')
+    const restartSign = this.add.image(this.game.scale.gameSize.width - 5, this.game.scale.gameSize.height+10, 'reload')
     .setOrigin(1).setScale((this.game.scale.gameSize.width / 6) / 224 * 2).setInteractive();
     restartSign.on('pointerdown', () => 
         {
@@ -218,8 +214,8 @@ export class MainScene extends Phaser.Scene {
     this.bombs = [];
     const bombs = bombPowerUps;
     for (let i = 0; i < bombs; i++) {
-      const bomb = this.add.image(i * this.tileWidth * 1.5 / 2 + this.tileWidth * 1.5 / 3,
-        this.game.scale.gameSize.height - 5 - this.feedbox!.height * this.feedbox!.scale / 2, 'feed')
+      const bomb = this.add.image(i * this.tileWidth * 1.5 / 1.7 + this.tileWidth * 1.5 / 1.7,
+        this.game.scale.gameSize.height - 5 - this.layer!.height * this.layer!.scale / 1.8, 'meteor')
         .setScale(this.assetScale * 1.5).setOrigin(0.5).setInteractive();
       (bomb as Phaser.GameObjects.Sprite).on('pointerdown', () => this.triggerBomb());
       this.bombs.push(bomb);
@@ -260,7 +256,7 @@ export class MainScene extends Phaser.Scene {
   private addTile(x: number, y: number) {
 
     const tileToAdd = this.tileTypes[this.random!.integerInRange(0, currentActiveTileTypes - 1)];
-    const tile = this.tiles!.create((x * this.tileWidth) + this.tileWidth / 2, 0, 'animals', tileToAdd);
+    const tile = this.tiles!.create((x * this.tileWidth) + this.tileWidth / 2, 0, 'dinos', tileToAdd);
     tile.scale = this.assetScale;
 
     this.add.tween({
